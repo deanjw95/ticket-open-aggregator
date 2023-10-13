d1bc23f416155378f840b0e25e035918da17ef6d
Jeong Wook Byun <deanjw95@naver.com>
2023-10-13 15:14:23 +0900

    initial commit;

---
+++ src/main/java/com/example/ticketopenaggregator/TicketopenaggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketopenaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketopenaggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketopenaggregatorApplication.class, args);
+	}
+
+}
