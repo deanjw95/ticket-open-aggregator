57de457f76572ec386c8a619b526eff22ffed626
Jeong Wook Byun <deanjw95@naver.com>
2023-10-20 09:43:11 +0900

    멜론 티켓 크롤링 테스트

--- src/test/java/com/example/ticketopenaggregator/CrawlingTest.java
+++ src/test/java/com/example/ticketopenaggregator/CrawlingTest.java
@@ -30,4 +30,32 @@ public class CrawlingTest {
         });
     }
 
+    @Test
+    public void testMelonTicketCrawling() throws IOException {
+        String url = "https://ticket.melon.com/csoon/ajax/listTicketOpen.htm";
+        int orderType = 2;
+        int pageIndex = 1;
+        String schGcode = "GENRE_ALL";
+
+        Document doc = Jsoup.connect(url)
+                .data("orderType", String.valueOf(orderType))
+                .data("pageIndex", String.valueOf(pageIndex))
+                .data("schGcode", schGcode)
+                .post();
+
+        doc.select(".txt_review").forEach(element -> {
+            // , 없애기
+            if (Integer.parseInt(element.text().replace(",", "")) > 1000) {
+                String date = element.parent().parent().select(".date").text();
+                Elements subject = element.parent().parent().select(".link_consert a");
+                String detailUrl = "https://ticket.melon.com/csoon/" + subject.attr("href").substring(2);
+
+                System.out.println("element = " + element.text());
+                System.out.println("date = " + date);
+                System.out.println("subject = " + detailUrl);
+            }
+        });
+    }
+
+
 }
